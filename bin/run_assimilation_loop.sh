e326e7d43067931b62ce481141255e6ee99f7131
xiaoyi <xiaoyi18@mails.tsinghua.edu.cn>
2025-06-01 13:16:55 +0800

    minor changes

--- bin/run_assimilation_loop.sh
+++ bin/run_assimilation_loop.sh
@@ -12,49 +12,49 @@ date='2025-05-31'
 # config='config/assimilation_loop/exp_3DVar.yaml'
 # mkdir experiments/assimilation/$prefix
 # cp $config experiments/assimilation/$prefix
-# srun -p ai4earth --quotatype=spot --ntasks-per-node=$single_gpus --cpus-per-task=$cpus -N $node_num -o job/%j.out --gres=gpu:$single_gpus --async -u python scripts/run_assimilation_loop.py --config $config --prefix $prefix --date $date
+# srun --ntasks-per-node=$single_gpus --cpus-per-task=$cpus -N $node_num -o job/%j.out --gres=gpu:$single_gpus --async -u python scripts/run_assimilation_loop.py --config $config --prefix $prefix --date $date
 
 # prefix='4dvar'
 # config='config/assimilation_loop/exp_4DVar.yaml'
 # mkdir experiments/assimilation/$prefix
 # cp $config experiments/assimilation/$prefix
-# srun -p ai4earth --quotatype=spot --ntasks-per-node=$single_gpus --cpus-per-task=$cpus -N $node_num -o job/%j.out --gres=gpu:$single_gpus --async -u python scripts/run_assimilation_loop.py --config $config --prefix $prefix --date $date
+# srun --ntasks-per-node=$single_gpus --cpus-per-task=$cpus -N $node_num -o job/%j.out --gres=gpu:$single_gpus --async -u python scripts/run_assimilation_loop.py --config $config --prefix $prefix --date $date
 
 # prefix='3denvar'
 # config='config/assimilation_loop/exp_3DEnVar.yaml'
 # mkdir experiments/assimilation/$prefix
 # cp $config experiments/assimilation/$prefix
-# srun -p ai4earth --quotatype=spot --ntasks-per-node=$single_gpus --cpus-per-task=$cpus -N $node_num -o job/%j.out --gres=gpu:$single_gpus --async -u python scripts/run_assimilation_loop.py --config $config --prefix $prefix --date $date
+# srun --ntasks-per-node=$single_gpus --cpus-per-task=$cpus -N $node_num -o job/%j.out --gres=gpu:$single_gpus --async -u python scripts/run_assimilation_loop.py --config $config --prefix $prefix --date $date
 
 # prefix='4denvar'
 # config='config/assimilation_loop/exp_4DEnVar.yaml'
 # mkdir experiments/assimilation/$prefix
 # cp $config experiments/assimilation/$prefix
-# srun -p ai4earth --quotatype=spot --ntasks-per-node=$single_gpus --cpus-per-task=$cpus -N $node_num -o job/%j.out --gres=gpu:$single_gpus --async -u python scripts/run_assimilation_loop.py --config $config --prefix $prefix --date $date
+# srun --ntasks-per-node=$single_gpus --cpus-per-task=$cpus -N $node_num -o job/%j.out --gres=gpu:$single_gpus --async -u python scripts/run_assimilation_loop.py --config $config --prefix $prefix --date $date
 
 prefix='vae-3dvar'
 config='config/assimilation_loop/exp_VAE-3DVar.yaml'
 mkdir experiments/assimilation/$prefix
 cp $config experiments/assimilation/$prefix
-srun -p ai4earth --quotatype=spot --ntasks-per-node=$single_gpus --cpus-per-task=$cpus -N $node_num -o job/%j.out --gres=gpu:$single_gpus --async -u python scripts/run_assimilation_loop.py --config $config --prefix $prefix --date $date
+srun --ntasks-per-node=$single_gpus --cpus-per-task=$cpus -N $node_num -o job/%j.out --gres=gpu:$single_gpus --async -u python scripts/run_assimilation_loop.py --config $config --prefix $prefix --date $date
 
 prefix='vae-4dvar'
 config='config/assimilation_loop/exp_VAE-4DVar.yaml'
 mkdir experiments/assimilation/$prefix
 cp $config experiments/assimilation/$prefix
-srun -p ai4earth --quotatype=spot --ntasks-per-node=$single_gpus --cpus-per-task=$cpus -N $node_num -o job/%j.out --gres=gpu:$single_gpus --async -u python scripts/run_assimilation_loop.py --config $config --prefix $prefix --date $date
+srun --ntasks-per-node=$single_gpus --cpus-per-task=$cpus -N $node_num -o job/%j.out --gres=gpu:$single_gpus --async -u python scripts/run_assimilation_loop.py --config $config --prefix $prefix --date $date
 
 prefix='lora-3denvar'
 config='config/assimilation_loop/exp_LoRA-3DEnVar.yaml'
 mkdir experiments/assimilation/$prefix
 cp $config experiments/assimilation/$prefix
-srun -p ai4earth --quotatype=spot --ntasks-per-node=$single_gpus --cpus-per-task=$cpus -N $node_num -o job/%j.out --gres=gpu:$single_gpus --async -u python scripts/run_assimilation_loop.py --config $config --prefix $prefix --date $date
+srun --ntasks-per-node=$single_gpus --cpus-per-task=$cpus -N $node_num -o job/%j.out --gres=gpu:$single_gpus --async -u python scripts/run_assimilation_loop.py --config $config --prefix $prefix --date $date
 
 prefix='lora-4denvar'
 config='config/assimilation_loop/exp_LoRA-4DEnVar.yaml'
 mkdir experiments/assimilation/$prefix
 cp $config experiments/assimilation/$prefix
-srun -p ai4earth --quotatype=spot --ntasks-per-node=$single_gpus --cpus-per-task=$cpus -N $node_num -o job/%j.out --gres=gpu:$single_gpus --async -u python scripts/run_assimilation_loop.py --config $config --prefix $prefix --date $date
+srun --ntasks-per-node=$single_gpus --cpus-per-task=$cpus -N $node_num -o job/%j.out --gres=gpu:$single_gpus --async -u python scripts/run_assimilation_loop.py --config $config --prefix $prefix --date $date
 
 sleep 2
 rm -f batchscript-*
